646ef1c7a18825e9a09dffcbd45a9cabc57ec8b1
Léo Sumi <18012247+leosumi@users.noreply.github.com>
2019-10-03 17:21:19 +0200

    Add comments

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ root=../leosumi.github.io
 p=/usr/bin/pandoc
 browser=/usr/bin/firefox
 
+# Check pandoc version
 installed=$($p --version | head -n 1 | cut -d " " -f 2)
 if [ "$version" != "$installed" ]; then
     echo "pandoc version:"
@@ -14,11 +15,13 @@ if [ "$version" != "$installed" ]; then
     exit 1
 fi
 
+# Root directory
 mkdir -p $root
 cp style.css $root/style.css
 cp -r res $root/
 $p -s -o $root/index.html index.md
 
+# Articles
 mkdir -p $root/articles
 $p -s -B templates/index-header.html -o $root/articles/index.html articles/index.md
 
@@ -28,19 +31,23 @@ $p -s -B templates/article-header.html -o $root/articles/a-small-report-about-pa
 $p -s -B templates/article-header.html -o $root/articles/the-free-software-road.html articles/the-free-software-road.md
 $p -s -B templates/article-header.html -o $root/articles/becoming-a-keyboard-master.html articles/becoming-a-keyboard-master.md
 
+# Chronicles
 mkdir -p $root/chronicles
 $p -s -B templates/index-header.html -o $root/chronicles/index.html chronicles/index.md
 
 $p -s -B templates/chronicle-header.html -o $root/chronicles/hello-world.html chronicles/hello-world.md
 
+# Lists
 mkdir -p $root/lists
 $p -s -B templates/index-header.html -o $root/lists/index.html lists/index.md
 
 $p -s -B templates/list-header.html -o $root/lists/todo-list.html lists/todo-list.md
 $p -s -B templates/list-header.html -o $root/lists/web-fiction-list.html lists/web-fiction-list.md
 
+# RSS
 pandoc -s -o $root/feed.xml --template templates/rss-template.xml feed.yml
 sed -i '/^$/d' $root/feed.xml
 xmllint --format $root/feed.xml --output $root/feed.xml
 
+# Display
 $browser $root/index.html
