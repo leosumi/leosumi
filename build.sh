2864be24341e6728052f2eae3c20857d061e1da0
Léo Sumi <18012247+leosumi@users.noreply.github.com>
2019-10-10 23:38:19 +0200

    Add a color theme selector

--- build.sh
+++ build.sh
@@ -17,32 +17,33 @@ fi
 
 # Root directory
 mkdir -p $root
-cp style.css $root/style.css
+cp -r css $root/
+cp -r js $root/
 cp -r res $root/
-$p -s -c style.css -o $root/index.html index.md
+$p -s -H templates/head.html -B templates/theme-settings.html -o $root/index.html index.md
 
 # Articles
 mkdir -p $root/articles
-$p -s -B templates/index-header.html -c ../style.css -o $root/articles/index.html articles/index.md
+$p -s -H templates/head.html -B templates/theme-settings.html -B templates/index-header.html -o $root/articles/index.html articles/index.md
 
-$p -s -B templates/article-header.html -c ../style.css -o $root/articles/how-to-learn-c.html articles/how-to-learn-c.md
-$p -s -B templates/article-header.html -c ../style.css -o $root/articles/rss-feeds-are-underused.html articles/rss-feeds-are-underused.md
-$p -s -B templates/article-header.html -c ../style.css -o $root/articles/a-small-report-about-patisiran.html articles/a-small-report-about-patisiran.md
-$p -s -B templates/article-header.html -c ../style.css -o $root/articles/the-free-software-road.html articles/the-free-software-road.md
-$p -s -B templates/article-header.html -c ../style.css -o $root/articles/becoming-a-keyboard-master.html articles/becoming-a-keyboard-master.md
+$p -s -H templates/head.html -B templates/theme-settings.html -B templates/article-header.html -o $root/articles/how-to-learn-c.html articles/how-to-learn-c.md
+$p -s -H templates/head.html -B templates/theme-settings.html -B templates/article-header.html -o $root/articles/rss-feeds-are-underused.html articles/rss-feeds-are-underused.md
+$p -s -H templates/head.html -B templates/theme-settings.html -B templates/article-header.html -o $root/articles/a-small-report-about-patisiran.html articles/a-small-report-about-patisiran.md
+$p -s -H templates/head.html -B templates/theme-settings.html -B templates/article-header.html -o $root/articles/the-free-software-road.html articles/the-free-software-road.md
+$p -s -H templates/head.html -B templates/theme-settings.html -B templates/article-header.html -o $root/articles/becoming-a-keyboard-master.html articles/becoming-a-keyboard-master.md
 
 # Chronicles
 mkdir -p $root/chronicles
-$p -s -B templates/index-header.html -c ../style.css -o $root/chronicles/index.html chronicles/index.md
+$p -s -H templates/head.html -B templates/theme-settings.html -B templates/index-header.html -o $root/chronicles/index.html chronicles/index.md
 
-$p -s -B templates/chronicle-header.html -c ../style.css -o $root/chronicles/hello-world.html chronicles/hello-world.md
+$p -s -H templates/head.html -B templates/theme-settings.html -B templates/chronicle-header.html -o $root/chronicles/hello-world.html chronicles/hello-world.md
 
 # Lists
 mkdir -p $root/lists
-$p -s -B templates/index-header.html -c ../style.css -o $root/lists/index.html lists/index.md
+$p -s -H templates/head.html -B templates/theme-settings.html -B templates/index-header.html -o $root/lists/index.html lists/index.md
 
-$p -s -B templates/list-header.html -c ../style.css -o $root/lists/todo-list.html lists/todo-list.md
-$p -s -B templates/list-header.html -c ../style.css -o $root/lists/web-fiction-list.html lists/web-fiction-list.md
+$p -s -H templates/head.html -B templates/theme-settings.html -B templates/list-header.html -o $root/lists/todo-list.html lists/todo-list.md
+$p -s -H templates/head.html -B templates/theme-settings.html -B templates/list-header.html -o $root/lists/web-fiction-list.html lists/web-fiction-list.md
 
 # RSS
 $p -s --template templates/rss-template.xml -o $root/feed.xml feed.yml
---
+++ js/theme.js
@@ -0,0 +1,3 @@
+function changeCSS(cssFile) {
+    document.getElementById("themeCSS").setAttribute("href", cssFile);
+}
